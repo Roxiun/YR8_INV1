3b49393008165e9573cdf211314fcc1f869d0d83
Roxiun <ronan.naidu@hotmail.com>
2020-08-01 19:58:22 +0800

    gitignore change

--- basic.py
+++ basic.py
@@ -9,7 +9,7 @@ from utils.data import ref
 
 Lighting_Condition = "Lightly Cloudy"
 Selected_ISO = "400"
-Selected_ShutterSpeed = "1/125"
+Selected_ShutterSpeed = "1/30"
 Selected_Aperture = ""
 SelectedSettings = {"Lighting":Lighting_Condition, "ISO":Selected_ISO, "Shutter":Selected_ShutterSpeed, "Aperture":Selected_Aperture}
 
