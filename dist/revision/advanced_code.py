8e6a55db5eb43e8fb85ab4c974cc38ae2ad1535b
Roxiun <ronan.naidu@hotmail.com>
2020-08-16 12:27:44 +0800

    big change

--- dist/revision/advanced_code.py
+++ dist/revision/advanced_code.py
@@ -1,4 +1,4 @@
-reference_numbers = {"1":{"Lighting":"Dusk","Iso":"50","Shutter":"1/4000","Aperture":"22"},"2":{"Lighting":"Sunset/Shade","Iso":"100","Shutter":"1/2000","Aperture":"16"},"3":{"Lighting":"Overcast","Iso":"200","Shutter":"1/1000","Aperture":"11"},"4":{"Lighting":"Cloudy","Iso":"400","Shutter":"1/500","Aperture":"8"},"5":{"Lighting":"Lightly Cloudy","Iso":"800","Shutter":"1/250","Aperture":"5.6"},"6":{"Lighting":"Sunny","Iso":"1600","Shutter":"1/125","Aperture":"4"},"7":{"Lighting":"Snow/Sand","Iso":"3200","Shutter":"1/60","Aperture":"2.8"},"8":{"Lighting":"-","Iso":"6400","Shutter":"1/30","Aperture":"2"}}; all_inputs = ["lighting", "iso", "shutter", "aperture"]; inputs = {}; inputs_aliases = {"lighting":"Lighting Condtion" ,"iso":"ISO", "aperture":"Aperture", "shutter":"Shutter Speed"}; modes = ["regular", "automatic", 'semi', "semi-automatic", "semi automatic"]; modes_aliases = {"regular":"regular","automatic":"automatic", "semi-automatic":"semi", "semi automatic": "semi", "semi":"semi"}; selected_mode = ""; semi_modes = ["portrait", "landscape"]; semi_select = "" # Creates a dictionary for the inputs ## Creates a dictionary that holds all the reference Numbers & Creates a list with all the inputs the user can enter
+reference_numbers = {"1":{"Lighting":"Dusk","Iso":"50","Shutter":"1/4000","Aperture":"22"},"2":{"Lighting":"Sunset/Shade","Iso":"100","Shutter":"1/2000","Aperture":"16"},"3":{"Lighting":"Overcast","Iso":"200","Shutter":"1/1000","Aperture":"11"},"4":{"Lighting":"Cloudy","Iso":"400","Shutter":"1/500","Aperture":"8"},"5":{"Lighting":"Lightly Cloudy","Iso":"800","Shutter":"1/250","Aperture":"5.6"},"6":{"Lighting":"Sunny","Iso":"1600","Shutter":"1/125","Aperture":"4"},"7":{"Lighting":"Snow/Sand","Iso":"3200","Shutter":"1/60","Aperture":"2.8"},"8":{"Lighting":"-","Iso":"6400","Shutter":"1/30","Aperture":"2"}}; all_inputs = ["lighting", "iso", "shutter", "aperture"]; inputs = {}; inputs_aliases = {"lighting":"Lighting Condtion" ,"iso":"ISO", "aperture":"Aperture", "shutter":"Shutter Speed"}; modes = ["regular", "automatic", 'semi', "semi-automatic", "semi automatic", "auto"]; modes_aliases = {"regular":"regular","automatic":"automatic", "auto":'automatic', "semi-automatic":"semi", "semi automatic": "semi", "semi":"semi"}; selected_mode = ""; semi_modes = ["portrait", "landscape"]; semi_select = "" # Creates a dictionary for the inputs ## Creates a dictionary that holds all the reference Numbers & Creates a list with all the inputs the user can enter
 def getResult(UserDict):
 	A=UserDict;B={}
 	for C in A:B[C]=getReferenceValue(A[C])
@@ -8,7 +8,7 @@ def getReferenceValue(UserInput):
         for value in reference_numbers[number]:
             if reference_numbers[number][value] == UserInput: return int(number) # Creates some functions to find the reference numbers
 while not selected_mode.lower() in modes: selected_mode = input("What mode would you like? Regular, Automatic or Semi-Automatic: ").lower()
-if modes_aliases[selected_mode.lower()] == "regular" or modes_aliases[selected_mode.lower()] == "semi":
+if modes_aliases[selected_mode.lower()] == "regular" or modes_aliases[selected_mode.lower()] == "automatic":
     user_selected = input("Enter that setting you want the program to recommend: ")
     while not user_selected.lower().replace("lighting condition", "lighting").replace("shutter speed", "shutter") in all_inputs: print("Thats not a valid condition"); user_selected = input("Enter that setting you want the program to recommend: ")  # Makes sure the user inputs a valid input
     for item in all_inputs:
@@ -26,8 +26,8 @@ if modes_aliases[selected_mode.lower()] == "regular" or modes_aliases[selected_m
         elif req_ref == 0: print(f"You need to change you settings by at least 1 stop")
     else:
         res[user_selected.lower().replace("lighting condition", "lighting").replace("shutter speed", "shutter")] = req_ref
-        while not semi_select in semi_modes: semi_select = input("Which Semi-Automatic Mode do you want (Portrait or Sports): ").lower()
+        while not semi_select in semi_modes: semi_select = input("Which Automatic Mode do you want (Portrait or Sports): ").lower()
         if semi_select == "portrait": res['iso'] = res['iso']-4; res['aperture'] = res['aperture']+4
         else: res['iso'] = res['iso']+1; res['aperture'] = res['aperture']+1; res['shutter'] = res['shutter']-1
         for result in res: print(f"Required {inputs_aliases[result.lower()]} value of: {reference_numbers[str(res[result])][result.title()]}")
-else: print("Automatic not dont yet sorry! :(")
+else: print("Semi-Automatic not dont yet sorry! :(")
